e478eabf1c82eb4094a697c91bdc50909296a6cb
GillisMcDonald <161117200+GillisMcDonald@users.noreply.github.com>
2024-04-25 22:58:14 -0700

    Add files via upload

--- Advamced Grid/Program.cs
+++ Advamced Grid/Program.cs
@@ -26,35 +26,31 @@ string[][] zoneTitles =
     ["Hell","Hell 2","Elesium","Styx","Core"],
 ];
 
-Vector2 bigGrid = new Vector2(0, 0);
-Vector2 charPos = new Vector2(0, 0);
-Vector2 prevPos;
-Vector2 charGrid = new Vector2(0, 0);
-Vector2 prevGrid = new Vector2(0, 0);
+Vector2 bigGridPos = new Vector2(0, 0);
+Vector2 charWorldPos = new Vector2(0, 0);
+Vector2 prevWorldPos = new Vector2(0, 0);
+Vector2 charGridPos = new Vector2(0, 0);
+Vector2 prevGridPos = new Vector2(0, 0);
 
 int bigGridCell = 0;
 
 Console.WriteLine($"WSAD to move, Space to place/remove. E to reset position. Q to reset canvas\nPress enter to begin.");
 while (true)
 {
-    prevPos = charPos;
-
     input = Console.ReadKey();
-    Console.Clear();
-    
     switch (input.Key.ToString())
     {
         case "W":
-            charPos.X--;
+            moveUp();
             break;
         case "S":
-            charPos.X++;
+            moveDown();
             break;
         case "A":
-            charPos.Y--;
+            moveLeft();
             break;
         case "D":
-            charPos.Y++;
+            moveRight();
             break;
         case "Spacebar":
             placeBomb();
@@ -66,25 +62,51 @@ while (true)
             resetCanvas();
             break;
     }
+    doMathStuff();
+    drawGrid();
+}
 
-    charPos.X = Math.Min(charPos.X, 49);
-    charPos.Y = Math.Min(charPos.Y, 49);
-    charPos.X = Math.Max(charPos.X, 0);
-    charPos.Y = Math.Max(charPos.Y, 0);
+void moveUp()
+{
+    charWorldPos.X--;
+    charWorldPos.X = Math.Max(charWorldPos.X, 0);
+}
 
-    bigGrid.X = (int)charPos.X / 10;
-    bigGrid.Y = (int)charPos.Y / 10;
+void moveDown()
+{
+    charWorldPos.X++;
+    charWorldPos.X = Math.Min(charWorldPos.X, 49);
+}
 
-    bigGridCell = ((int)bigGrid.X * 5) + (int)bigGrid.Y;
+void moveLeft()
+{
+    charWorldPos.Y--;
+    charWorldPos.Y = Math.Max(charWorldPos.Y, 0);
+}
 
-    prevGrid.X = prevPos.X % 10;
-    prevGrid.Y = prevPos.Y % 10;
+void moveRight()
+{
+    charWorldPos.Y++;
+    charWorldPos.Y = Math.Min(charWorldPos.Y, 49);
+}
 
-    charGrid.X = charPos.X % 10;
-    charGrid.Y = charPos.Y % 10;
+void doMathStuff()
+{
+    bigGridPos.X = (int)charWorldPos.X / 10;
+    bigGridPos.Y = (int)charWorldPos.Y / 10;
+    bigGridCell = ((int)bigGridPos.X * 5) + (int)bigGridPos.Y;
+    prevGridPos.X = prevWorldPos.X % 10;
+    prevGridPos.Y = prevWorldPos.Y % 10;
+    charGridPos.X = charWorldPos.X % 10;
+    charGridPos.Y = charWorldPos.Y % 10;
+    grid[(int)prevGridPos.X][(int)prevGridPos.Y] -= charValue;
+    grid[(int)charGridPos.X][(int)charGridPos.Y] += charValue;
+    prevWorldPos = charWorldPos;
+}
 
-    grid[(int)prevGrid.X][(int)prevGrid.Y] -= charValue;
-    grid[(int)charGrid.X][(int)charGrid.Y] += charValue;
+void drawGrid()
+{
+    Console.Clear();
     Console.WriteLine("▄▄▄▄▄▄▄▄▄▄▄▄");
     for (int i = 0; i < grid.Length; i++)
     {
@@ -107,12 +129,12 @@ while (true)
         Console.WriteLine("█");
     }
     Console.WriteLine("▀▀▀▀▀▀▀▀▀▀▀▀");
-    Console.WriteLine(zoneTitles[(uint)bigGrid.X][(uint)bigGrid.Y]);
+    Console.WriteLine(zoneTitles[(uint)bigGridPos.X][(uint)bigGridPos.Y]);
 }
 
 void placeBomb()
 {
-    grid[(int)charGrid.X][(int)charGrid.Y] = grid[(int)charGrid.X][(int)charGrid.Y] ^ (uint)1 << bigGridCell;
+    grid[(int)charGridPos.X][(int)charGridPos.Y] = grid[(int)charGridPos.X][(int)charGridPos.Y] ^ (uint)1 << bigGridCell;
 }
 
 bool isBombHere(uint gridValue, int cell)
@@ -126,8 +148,8 @@ bool isBombHere(uint gridValue, int cell)
 
 void resetPosition()
 {
-    charPos.X = 0;
-    charPos.Y = 0;
+    charWorldPos.X = 0;
+    charWorldPos.Y = 0;
 }
 
 void resetCanvas()
@@ -136,7 +158,7 @@ void resetCanvas()
     {
         for (int j = 0; j < grid[i].Length; j++)
         {
-            if(grid[i][j] >= charValue)
+            if (grid[i][j] >= charValue)
             {
                 grid[i][j] = charValue;
             }
